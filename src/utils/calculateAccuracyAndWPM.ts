392613afa53f793cb62af920b8542c90fcf1631e
heyimaaditya <aadityatyagi158@gmail.com>
2023-11-16 11:39:50 +0530

    calculate accuracy,words per minute

--- src/utils/calculateAccuracyAndWPM.ts
+++ src/utils/calculateAccuracyAndWPM.ts
@@ -1,28 +1,28 @@
-const calculateWordsPerMinute = (
-  originalText: string,
-  currentText: string,
-  timeInMinutes: number
-) => {
-  const currentWords = currentText.trim().split(/\s+/);
-  const originalWords = originalText
-    .trim()
-    .split(/\s+/)
-    .slice(0, currentWords.length);
-
-  let correctWords = 0;
-  for (let i = 0; i < currentWords.length; i++) {
-    if (originalWords.includes(currentWords[i])) {
+const calculateWordsPerMinute=(
+  originalText:string,
+  currentText:string,
+  timeInMinutes:number
+)=>{
+  const currentWords=currentText.trim().split(/\s+/);
+  const originalWords=originalText.trim().split(/\s+/).slice(0,currentWords.length);
+  const correctWordsArray=[];
+  const incorrectWordsArray=[];
+  let correctWords=0;
+  for(let i=0;i<currentWords.length;i++){
+    if(originalWords.includes(currentWords[i])){
       correctWords++;
+      correctWordsArray.push(currentWords[i]);
+    }
+    else{
+      incorrectWordsArray.push(currentWords[i]);
     }
   }
-
   const wordsPerMinute = parseInt((correctWords / timeInMinutes).toFixed(2));
-
   const accuracy = parseInt(
     ((correctWords / currentWords.length) * 100).toFixed(2)
   );
 
-  return { wordsPerMinute, accuracy };
+  return { wordsPerMinute, accuracy, correctWordsArray, incorrectWordsArray };
 };
 
 export default calculateWordsPerMinute;
